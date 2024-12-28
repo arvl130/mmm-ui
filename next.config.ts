c63381faad5305c12be90ecc09a42d8c4c81b49e
Angelo Geulin <angelogeulin123@gmail.com>
2024-12-28 12:56:28 +0800

    feat: forward requests to /api/*

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import { serverEnv } from "./src/server/env"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${serverEnv.BACKEND_URL}/:path*`, // Proxy all requests to /api/* to backend.
+      },
+    ]
+  },
 }
 
 export default nextConfig
