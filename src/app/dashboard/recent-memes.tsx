9ed74d9ec4951a8b9feb5647291fe473db12cdf1
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-18 14:56:08 +0800

    refactor: move search input to its own form

--- src/app/dashboard/recent-memes.tsx
+++ src/app/dashboard/recent-memes.tsx
@@ -2,12 +2,11 @@
 
 import { Button } from "@/components/ui/button"
 import { Loader2, TriangleAlert } from "lucide-react"
-import { Suspense, useCallback } from "react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { Suspense } from "react"
+import { useSearchParams } from "next/navigation"
 import { useMemes } from "@/hooks/memes"
 import { MemeList } from "./meme-list"
-import { Input } from "@/components/ui/input"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { SearchForm } from "./search-form"
 
 type TSearchMode = "SIMPLE" | "FULL_TEXT" | "SEMANTIC"
 
@@ -16,8 +15,6 @@ export function RecentMemes({
 }: {
   onOpenUploadModal: () => void
 }) {
-  const router = useRouter()
-  const pathname = usePathname()
   const searchParams = useSearchParams()
   const searchTerm = searchParams.get("q")
   const searchMode = searchParams.get("mode")
@@ -26,48 +23,13 @@ export function RecentMemes({
     mode: searchMode === null ? undefined : (searchMode as TSearchMode),
   })
 
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
-
-      return params.toString()
-    },
-    [searchParams],
-  )
-
   return (
     <Suspense>
       <h2 className="text-2xl font-semibold">Recent memes</h2>
       <p className="mt-1 text-muted-foreground text-sm">
         Browse your recently uploaded memes.
       </p>
-      <div className="grid grid-cols-[auto_1fr] gap-2 mt-2">
-        <Tabs
-          defaultValue={
-            searchMode === null ? "SIMPLE" : (searchMode as TSearchMode)
-          }
-          onValueChange={(value) => {
-            router.push(pathname + "?" + createQueryString("mode", value))
-          }}
-        >
-          <TabsList>
-            <TabsTrigger value="SEMANTIC">AI</TabsTrigger>
-            <TabsTrigger value="FULL_TEXT">Full Text</TabsTrigger>
-            <TabsTrigger value="SIMPLE">Simple</TabsTrigger>
-          </TabsList>
-        </Tabs>
-
-        <Input
-          type="search"
-          placeholder="Enter a tag ..."
-          onChange={(e) => {
-            router.push(
-              pathname + "?" + createQueryString("q", e.currentTarget.value),
-            )
-          }}
-        />
-      </div>
+      <SearchForm />
       {memes.status === "pending" && (
         <div className="mt-4 flex">
           <Loader2 className="animate-spin mr-2" /> Loading ...
---
+++ src/app/dashboard/search-form.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useCallback, useState } from "react"
+
+type TSearchMode = "SIMPLE" | "FULL_TEXT" | "SEMANTIC"
+
+export function SearchForm() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const [searchTerm, setSearchTerm] = useState(searchParams.get("q") ?? "")
+  const [searchMode, setSearchMode] = useState<TSearchMode>(
+    (searchParams.get("mode") as TSearchMode) ?? "SIMPLE",
+  )
+
+  const createQueryString = useCallback(
+    ({ keyPairs }: { keyPairs: { name: string; value: string }[] }) => {
+      const params = new URLSearchParams(searchParams.toString())
+
+      for (const keyPair of keyPairs) {
+        params.set(keyPair.name, keyPair.value)
+      }
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  return (
+    <form
+      className="grid grid-cols-[1fr_auto_auto] gap-2 mt-2"
+      onSubmit={(e) => {
+        e.preventDefault()
+        router.push(
+          pathname +
+            "?" +
+            createQueryString({
+              keyPairs: [
+                {
+                  name: "q",
+                  value: searchTerm,
+                },
+                {
+                  name: "mode",
+                  value: searchMode,
+                },
+              ],
+            }),
+        )
+      }}
+    >
+      <Input
+        type="search"
+        placeholder="Enter a tag ..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.currentTarget.value)
+        }}
+      />
+
+      <Tabs
+        value={searchMode}
+        onValueChange={(value) => {
+          setSearchMode(value as TSearchMode)
+        }}
+      >
+        <TabsList>
+          <TabsTrigger value="SIMPLE">Simple</TabsTrigger>
+          <TabsTrigger value="SEMANTIC">AI</TabsTrigger>
+          <TabsTrigger value="FULL_TEXT">Full Text</TabsTrigger>
+        </TabsList>
+      </Tabs>
+
+      <Button type="submit">Search</Button>
+    </form>
+  )
+}
