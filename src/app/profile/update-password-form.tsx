f8e03ed0dbec699421e5fed1638477bf047453c7
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-19 19:59:38 +0800

    style: add spacing

--- src/app/profile/update-password-form.tsx
+++ src/app/profile/update-password-form.tsx
@@ -79,7 +79,7 @@ export function UpdatePasswordForm() {
           })
         }}
       >
-        <CardContent>
+        <CardContent className="space-y-4">
           <div className="space-y-2">
             <Label>Old Password</Label>
             <Input
